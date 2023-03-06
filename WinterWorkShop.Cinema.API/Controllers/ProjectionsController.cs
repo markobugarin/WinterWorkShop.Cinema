87c97ad8e5c15fa362d24803c3798ce4781db548
m.bugarin-int <markobugarin02@gmail.com>
2023-03-06 14:54:43 +0100

    Created a Projection Controller

---
+++ WinterWorkShop.Cinema.API/Controllers/ProjectionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WinterWorkShop.Cinema.API.Controllers
+{
+    public class ProjectionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
